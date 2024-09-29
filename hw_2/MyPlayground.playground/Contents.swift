4be92b372fc803cc8d80ad3850de2805c29f6794
Nur-Magomed <nur-magomed@mail.ru>
2024-09-29 13:30:54 +0300

    finished second hw

--- hw_2/MyPlayground.playground/Contents.swift
+++ hw_2/MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// Сгенерировать массив случайных чисел (например 200 чисел). Найти индекс первого повторяющегося числа в массиве. Если все числа разные - то -1.
+
+var randomNumArray: Array<Int> = [Int](repeating: 0, count: 200)
+for index in 0..<randomNumArray.count {
+    randomNumArray[index] = Int.random(in: 0...1000)
+}
+
+var numbSet: Set<Int> = Set()
+var firstRepeatedIndex = -1
+for index in 0..<randomNumArray.count {
+    if (numbSet.contains(randomNumArray[index]) && firstRepeatedIndex == -1) {
+        firstRepeatedIndex = index
+    } else {
+        numbSet.insert(randomNumArray[index])
+    }
+}
+
+print(firstRepeatedIndex)
+        
