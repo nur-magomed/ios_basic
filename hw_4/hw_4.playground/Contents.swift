d0397c381cfd83ac92c055cc516fc34d908fcb63
nur-magomed <nur-magomed@mail.ru>
2024-11-25 09:03:58 +0300

    finished hw4

--- hw_4/hw_4.playground/Contents.swift
+++ hw_4/hw_4.playground/Contents.swift
@@ -1,3 +1,23 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct User {
+    
+    var name: String
+    let age: Int
+    let email: String
+    
+}
+
+    
+var user1 = User(name: "John", age: 18, email: "john@mail.com")
+var user2 = User(name: "Jane", age: 35, email: "jane@mail.com")
+var user3 = User(name: "Alex", age: 42, email: "alex@mail.com")
+
+var users: [User] = [user1, user2, user3]
+print(users)
+users[1].name = "Alice"
+print(users)
+
+
+
+
