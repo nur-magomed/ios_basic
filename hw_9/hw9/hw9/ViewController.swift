6ef0baa8215b3eecbb13af968fa45f6d2e411f35
nur-magomed <nur-magomed@mail.ru>
2024-12-08 07:45:40 +0300

    init hw9

---
+++ hw_9/hw9/hw9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw9
+//
+//  Created by Nur-Magomed Arsakhanov on 08.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw_9/hw9/hw9Tests/hw9Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hw9Tests.swift
+//  hw9Tests
+//
+//  Created by Nur-Magomed Arsakhanov on 08.12.2024.
+//
+
+import Testing
+@testable import hw9
+
+struct hw9Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
