fd66e165bd30774936d4094fd0763fb1cf2ae79e
nur-magomed <nur-magomed@mail.ru>
2025-01-04 14:55:55 +0300

    hw10 init

---
+++ hw_10/HW10/HW10Tests/HW10Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW10Tests.swift
+//  HW10Tests
+//
+//  Created by Nur-Magomed Arsakhanov on 04.01.2025.
+//
+
+import Testing
+@testable import HW10
+
+struct HW10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
