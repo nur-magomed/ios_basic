37cbf1274cb7d693b1ec2d9f45ba6bb98f1c98d2
nur-magomed <nur-magomed@mail.ru>
2025-01-25 19:47:20 +0300

    init hm 11

---
+++ hw_11/HW11/HW11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW11
+//
+//  Created by Nur-Magomed Arsakhanov on 25.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw_11/HW11/HW11Tests/HW11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW11Tests.swift
+//  HW11Tests
+//
+//  Created by Nur-Magomed Arsakhanov on 25.01.2025.
+//
+
+import Testing
+@testable import HW11
+
+struct HW11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
