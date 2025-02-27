2d7f0e089bf1a0cb88ed1689b8343b468b2beec9
nur-magomed <nur-magomed@mail.ru>
2025-02-27 18:29:37 +0300

    init hw16

---
+++ hw_16/hw16/hw16Tests/hw16Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hw16Tests.swift
+//  hw16Tests
+//
+//  Created by Nur-Magomed Arsakhanov on 27.02.2025.
+//
+
+import Testing
+@testable import hw16
+
+struct hw16Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ hw_16/hw16/hw16UITests/hw16UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  hw16UITestsLaunchTests.swift
+//  hw16UITests
+//
+//  Created by Nur-Magomed Arsakhanov on 27.02.2025.
+//
+
+import XCTest
+
+final class hw16UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
