e3c88b3dcecbb779c591e503a6427b43db7ba336
nur-magomed <nur-magomed@mail.ru>
2025-02-28 23:39:05 +0300

    finished hw18

--- hw_18/State.playground/Contents.swift
+++ hw_18/State.playground/Contents.swift
@@ -1,3 +1,67 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, state"
+enum State: CaseIterable {
+    case orderInitiated
+    case stockReduced
+    case fundsTaken
+    case emailSent
+    case orderCompleted
+    case stockRestored
+    case refundIssued
+    case rollbacked
+}
+
+
+protocol StateItem {
+    associatedtype ItemState: Hashable
+
+    var state: ItemState { get }
+
+    static var transactions: [ItemState: Set<ItemState>] { get }
+
+    func canTransform(nextState: ItemState) -> Bool
+
+    mutating func transition(nextState: ItemState)
+}
+
+extension StateItem {
+    static func isStateAllowed(from: ItemState, to: ItemState) -> Bool {
+        guard let validStates = transactions[from] else { fatalError() }
+        return validStates.contains(to)
+    }
+
+    func canTransform(nextState: ItemState) -> Bool {
+        return type(of: self).isStateAllowed(from: state, to: nextState)
+    }
+}
+
+
+class GiftRedeemHistory: StateItem {
+    private(set) var state: State = .orderInitiated
+
+    static let transactions: [State : Set<State>] = [
+        .orderInitiated: [State.stockReduced, State.fundsTaken],
+        .stockReduced: [State.fundsTaken, State.stockRestored, State.rollbacked],
+        .fundsTaken: [State.stockRestored, State.refundIssued, State.emailSent, State.rollbacked],
+        .emailSent: [State.orderCompleted],
+        .refundIssued: [State.stockRestored, State.rollbacked],
+        .stockRestored: [State.rollbacked]
+    ]
+
+    func transition(nextState: State) {
+        precondition(canTransform(nextState: nextState), "Wrong state")
+        self.state = nextState
+    }
+}
+
+
+do {
+    var redeemHistory = GiftRedeemHistory()
+    print(redeemHistory.state)
+    redeemHistory.transition(nextState: .stockReduced)
+    print(redeemHistory.state)
+    redeemHistory.transition(nextState: .fundsTaken)
+    print(redeemHistory.state)
+    redeemHistory.transition(nextState: .emailSent)
+    print(redeemHistory.state)
+}
