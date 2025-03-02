1e1ece5db641209a169cb340759ac1f1d37fa75b
nur-magomed <nur-magomed@mail.ru>
2025-03-02 11:19:35 +0300

    init hw13

---
+++ hw_13/hw13network/hw13network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw13network
+//
+//  Created by Nur-Magomed Arsakhanov on 01.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw_13/hw13network/hw13networkTests/hw13networkTests.swift
@@ -0,0 +1,17 @@
+//
+//  hw13networkTests.swift
+//  hw13networkTests
+//
+//  Created by Nur-Magomed Arsakhanov on 01.03.2025.
+//
+
+import Testing
+@testable import hw13network
+
+struct hw13networkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
