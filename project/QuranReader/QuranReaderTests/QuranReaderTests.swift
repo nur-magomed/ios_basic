338cc22858504b0c84a61da0bc43ed1af06f5cfb
nur-magomed <nur-magomed@mail.ru>
2025-03-15 23:06:28 +0300

    init project

---
+++ project/QuranReader/QuranReaderTests/QuranReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuranReaderTests.swift
+//  QuranReaderTests
+//
+//  Created by Nur-Magomed A on 15.03.2025.
+//
+
+import Testing
+@testable import QuranReader
+
+struct QuranReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
